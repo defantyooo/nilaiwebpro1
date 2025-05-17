23c0d4562d56f5982f738cf42e8709f4a9a742d2
Defantyo Shabian Hadi <defanshad@gmail.com>
2025-05-17 08:21:00 +0700

    Add files via upload

---
+++ siswa/delete.php
@@ -0,0 +1,11 @@
+<?php
+include_once('config.php');
+$id = $_GET['id'];
+
+$sql = "DELETE FROM students WHERE id='$id'";
+$result = mysqli_query($conn, $sql);
+if ($result) {
+    header("Location: ?m=siswa&sm=view");
+} else {
+    echo "<script>alert('Data gagal dihapus'); window.location='?m=siswa&s=view';</script>";
+}
