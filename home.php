29a6387320341a3af78a035eb393cbc2b2642161
Defantyo Shabian Hadi <defanshad@gmail.com>
2025-05-17 08:21:16 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,10 @@
+<div class="card">
+    <div class="card-body">
+        <p>
+            <img src="img\Mercedes AMG.png" alt="wkw" width="400" height="120">
+            <h1>Aplikasi Nilai Siswa</h1>
+            <h3>Web Programming</h3>
+            <p>Aplikasi ini dirancang untuk memudahkan penyimpanan, pengelolaan, dan pemantauan data siswa di tingkat Sekolah Menengah Atas (SMA). Sistem ini mencakup fitur utama seperti pencatatan data identitas siswa, riwayat akademik, kehadiran, pelanggaran, prestasi, serta data bimbingan konseling. Aplikasi ini memungkinkan pihak sekolah, seperti wali kelas, guru BK, dan kepala sekolah, untuk mengakses informasi siswa secara cepat dan terintegrasi guna mendukung proses pendidikan dan pengambilan keputusan. Dengan antarmuka yang user-friendly dan sistem keamanan data yang terjamin, aplikasi ini dapat digunakan baik secara online maupun offline. Selain itu, aplikasi ini dilengkapi fitur pencarian cepat, pelaporan otomatis, serta notifikasi untuk aktivitas penting siswa seperti keterlambatan, izin, atau jadwal konseling.</p>
+        </p>
+    </div>
+</div>
